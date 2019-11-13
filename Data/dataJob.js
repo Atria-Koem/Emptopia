aa410de1e0dc59696ec02b5cf5b3931f7db89212
Min-KH114 <56007365+Min-KH114@users.noreply.github.com>
2019-11-14 05:48:07 +0900

    Update dataJob.js
    스탯 조정 + 직업 추가

--- Data/dataJob.js
+++ Data/dataJob.js
@@ -30,7 +30,7 @@ const  dataJob = {
 		code : 'J30000000',
 		name : 'Priest',
 		state : {
-			str: 100,				int: 101,				dex: 100,				dis : 100,				spd: 100,
+			str: 100,				int: 102,				dex: 100,				dis : 100,				spd: 100,
 			vit: 100,				wis: 100,				agi: 100,				res : 101,				luk: 100
 		},
 		limit : []
@@ -148,7 +148,7 @@ const  dataJob = {
 	  name : 'Dancer', 
 	 state : {
  		str : 100, 	vit : 100, 	int : 101, 	wis : 100, 	dex : 101, 	
- 		agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+ 		agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 101
 	}, 
 	  limit : ['Sword', 'TwoHandSword', 'Pike', 'Spear', 'Hatchet', 'Axe', 'Mace', 'Pistol', 'Gun', 'Wand', 'Staff', 'Bow', 'MainGauche', 'SubSword', 'SubSword', 'SubPike', 'SubHatchet', 'SubMace', 'SubPistol', 'BowGun', 'Shield', 'Book'], 
 	  exp : '춤으로 적을 현혹시키는 서포터' 
@@ -177,7 +177,7 @@ const  dataJob = {
 		  code : 'J11000000', 
 		  name : 'Berserker', 
 		 state : {
-			str : 103, 	vit : 101, 	int : 98, 	wis : 100, 	dex : 100, 	
+			str : 104, 	vit : 101, 	int : 98, 	wis : 100, 	dex : 100, 	
 			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
 		}, 
 		  limit : ['Shield', 'Book', 'Ammor', 'Cloth', 'Robe'], 
@@ -187,7 +187,7 @@ const  dataJob = {
 		  code : 'J12000000', 
 		  name : 'Squire', 
 		 state : {
-			str : 101, 	vit : 101, 	int : 100, 	wis : 100, 	dex : 100, 	
+			str : 101, 	vit : 102, 	int : 100, 	wis : 100, 	dex : 100, 	
 			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
 		}, 
 		  limit : [], 
@@ -197,7 +197,7 @@ const  dataJob = {
 		  code : 'J12100000', 
 		  name : 'Apprentice Knight', 
 		 state : {
-			str : 102, 	vit : 101, 	int : 100, 	wis : 100, 	dex : 100, 	
+			str : 102, 	vit : 102, 	int : 100, 	wis : 100, 	dex : 100, 	
 			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
 		}, 
 		  limit : [], 
@@ -207,7 +207,7 @@ const  dataJob = {
 		  code : 'J12110000', 
 		  name : 'Knight', 
 		 state : {
-			str : 102, 	vit : 102, 	int : 100, 	wis : 100, 	dex : 100, 	
+			str : 102, 	vit : 103, 	int : 100, 	wis : 100, 	dex : 100, 	
 			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
 		}, 
 		  limit : [], 
@@ -263,4 +263,154 @@ const  dataJob = {
 		  limit : ['Sword', 'TwoHandSword', 'Pike', 'Spear', 'Hatchet', 'Axe', 'Mace', 'Pistol', 'Gun', 'Wand', 'Staff', 'Bow', 'MainGauche', 'SubSword', 'SubSword', 'SubPike', 'SubHatchet', 'SubMace', 'SubPistol', 'BowGun', 'Shield', 'Book'],  
 		  exp : '' 
 	},
+	J13000000 : { 
+		  code : 'J13000000', 
+		  name : 'Barbarian', 
+		 state : {
+			str : 100, 	vit : 103, 	int : 100, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['Dagger', 'Hatchet', 'Pistol', 'Gun', 'Staff', 'Bow', 'Whip', 'MainGauche', 'SubHatchet', 'SubPistol', 'BowGun', 'Book', 'Cloth', 'Robe'], 
+		  exp : '아군의 사기를 올려주는 함성을 지르곤 한다.' 
+	},
+	J14000000 : { 
+		  code : 'J14000000', 
+		  name : 'Gladiator', 
+		 state : {
+			str : 101, 	vit : 101, 	int : 100, 	wis : 100, 	dex : 101, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['Pistol', 'Gun', 'Staff', 'Bow', 'Whip', 'SubPistol', 'BowGun', 'Book', 'ArrowCase', 'Cloth', 'Robe'], 
+		  exp : '싸움에만 집중하는 이들은 검술실력은 물론 싸움기술마저 훌륭하다.' 
+	},
+	J15000000 : { 
+		  code : 'J15000000', 
+		  name : 'Wrestler', 
+		 state : {
+			str : 98, 	vit : 103, 	int : 100, 	wis : 100, 	dex : 102, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['Sword', 'TwoHandSword', 'Dagger', 'Pike', 'Spear', 'Hatchet', 'Axe', 'Mace', 'Pistol', 'Gun', 'Wand', 'Staff', 'Bow', 'Whip', 'MainGauche', 'SubSword', 'SubSword', 'SubPike', 'SubHatchet', 'SubMace', 'SubPistol', 'BowGun', 'Shield', 'Book', 'ArmGaurd', 'ArrowCase'], 
+		  exp : '무기술을 사용하지 않는 이들은 오로지 맨몸과 기술만으로 적들을 제압한다.' 
+	},
+	J16000000 : { 
+		  code : 'J16000000', 
+		  name : 'Spearman', 
+		 state : {
+			str : 100, 	vit : 101, 	int : 100, 	wis : 100, 	dex : 102, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['Sword', 'TwoHandSword', 'Dagger', 'Hatchet', 'Axe', 'Mace', 'Pistol', 'Gun', 'Wand', 'Staff', 'Bow', 'Whip', 'MainGauche', 'SubSword', 'SubSword', 'SubMace', 'SubPistol', 'BowGun', 'Shield', 'Book', 'ArmGaurd', 'ArrowCase'], 
+		  exp : '창을 주무기로 사용하는 이들은 전투에서 자신들의 무기의 길이를 이용할 줄 알고있다.' 
+	},
+	J32000000 : { 
+		  code : 'J32000000', 
+		  name : 'Holy Knight', 
+		 state : {
+			str : 100, 	vit : 102, 	int : 101, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['TwoHandSword', 'Dagger', 'Hatchet', 'Pistol', 'Gun', 'Wand', 'Staff', 'Bow', 'Whip', 'SubSword', 'SubSword', 'SubMace', 'SubPistol', 'BowGun', 'Book', 'ArmGaurd', 'ArrowCase'], 
+		  exp : '전위에서 방어를 담당하는 신성기사는 팀원의 체력을 책임지고는 한다.' 
+	},
+	J33000000 : { 
+		  code : 'J33000000', 
+		  name : 'Punishment Knight', 
+		 state : {
+			str : 102, 	vit : 100, 	int : 101, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['TwoHandSword', 'Dagger', 'Hatchet', 'Pistol', 'Gun', 'Wand', 'Staff', 'Bow', 'Whip', 'SubSword', 'SubSword', 'SubMace', 'SubPistol', 'BowGun', 'Book', 'ArmGaurd', 'ArrowCase'], 
+		  exp : '신성력을 통해 강력한 공격을 하는 징벌기사들은 악행을 저지르고 도망치는 이들을 쫓고 있다고 한다.' 
+	},
+	J34000000 : { 
+		  code : 'J34000000', 
+		  name : 'Protector', 
+		 state : {
+			str : 100, 	vit : 100, 	int : 103, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['TwoHandSword', 'Dagger', 'Hatchet', 'Pistol', 'Gun', 'Wand', 'Staff', 'Bow', 'Whip', 'SubSword', 'SubSword', 'SubMace', 'SubPistol', 'BowGun', 'Book', 'ArmGaurd', 'ArrowCase'], 
+		  exp : '신성력을 통해 보호의 힘을 사용하는 이들은 방어력 상승 오라를 비롯한 전투에 도움되는 각종 오라를 통해 동료들을 강화해준다.' 
+	},
+	J55000000 : { 
+		  code : 'J55000000', 
+		  name : 'Squad Leader', 
+		 state : {
+			str : 101, 	vit : 102, 	int : 102, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 90
+		}, 
+		  limit : [], 
+		  exp : '열명의 용병을 이끄는 십부장.' 
+	},
+	J55100000 : { 
+		  code : 'J55100000', 
+		  name : 'Centurion', 
+		 state : {
+			str : 101, 	vit : 102, 	int : 103, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 90
+		}, 
+		  limit : [], 
+		  exp : '열명의 십부장을 이끄는 백부장.' 
+	},J55110000 : { 
+		  code : 'J55110000', 
+		  name : 'Patriarchy', 
+		 state : {
+			str : 101, 	vit : 102, 	int : 104, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 90
+		}, 
+		  limit : [], 
+		  exp : '열명의 백부장을 이끄는 천부장.' 
+	},
+	J55111000 : { 
+		  code : 'J55111000', 
+		  name : 'Commander', 
+		 state : {
+			str : 101, 	vit : 102, 	int : 105, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 90
+		}, 
+		  limit : [], 
+		  exp : '전장에서 용병들을 직접 지휘하는 지휘관들은 아군의 사기가 떨어지지 않도록하며 계책을 통해 전투를 유리하게 만든다.' 
+	},
+	J55111100 : { 
+		  code : 'J55111100', 
+		  name : 'Captain', 
+		 state : {
+			str : 102, 	vit : 102, 	int : 105, 	wis : 100, 	dex : 100, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 90
+		}, 
+		  limit : [], 
+		  exp : '하나의 집단을 이끄는 이들을 지칭한다.' 
+	},
+	J44000000 : { 
+		  code : 'J44000000', 
+		  name : 'Investigator', 
+		 state : {
+			str : 100, 	vit : 100, 	int : 100, 	wis : 100, 	dex : 102, 	
+			agi : 101, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : [], 
+		  exp : '흔적을 찾는 조사단의 일원.' 
+	},
+	J44100000 : { 
+		  code : 'J44100000', 
+		  name : 'Blue Star', 
+		 state : {
+			str : 100, 	vit : 100, 	int : 100, 	wis : 100, 	dex : 103, 	
+			agi : 101, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : [], 
+		  exp : '조사단원 중 가장 특출난 이를 지칭하곤 한다.' 
+	},
+	J45000000 : { 
+		  code : 'J45000000', 
+		  name : 'Theif', 
+		 state : {
+			str : 100, 	vit : 100, 	int : 100, 	wis : 100, 	dex : 103, 	
+			agi : 100, 	dis : 100, 	res : 100, 	spd : 100, 	luk : 100
+		}, 
+		  limit : ['TwoHandSword', 'Pike', 'Spear', 'Staff', 'Whip', 'Ammor', 'Helmet'], 
+		  exp : '이 도둑들은 적이 가지고 있는 아이템을 무료로 훔쳐줍니다.' 
+	},
+
 }
